404ad2d7f011d56766010cb6de0188bf09f26ad1
numbers24 <45888417+numbers24@users.noreply.github.com>
2019-12-10 01:42:50 -0500

    Update DUMBserver.c

--- DUMBserver.c
+++ DUMBserver.c
@@ -5,10 +5,118 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #define PORT 6654
-int main(int argc, char const *argv[]) 
+
+typedef struct box
+{
+	char* name;
+	char* message;
+	struct box* next;
+}box;
+
+box* list;
+int sockets[256];
+
+void add(box* new)
+{
+	struct box* ptr;
+	for(ptr=list;ptr!=NULL;ptr=ptr->next)
+	{
+		if(ptr->next==NULL)
+		{
+			ptr->next = new;
+			return;
+		}
+	}
+}
+box* getBox(char* name)
+{
+	box* curr;
+	for(curr=list;curr!=NULL;curr=curr->next)
+	{
+		if(!strcmp(curr->name,name))
+		return curr;
+	}
+	return NULL;
+}
+
+//Commands
+
+void create(char* name)
+{
+	/*
+		rules:
+		1.) 5-25 characters
+		2.) must start with an alphabetic character
+	*/
+
+	char* response;
+	send(newSock,response,strlen(response),0);
+	
+	if(name==NULL)
+	{
+		response="You need a name, dude.";
+		send(newSock,response,strlen(response),0);
+		return;
+	}
+	if(!isalpha(name[0])
+	{
+		response="You need to start your name with an alphabetical character.";
+		send(newSock,response,strlen(response),0);
+		return;
+	}
+	if(strlen(name)<5||strlen(name)>25)
+	{
+		response="Your name must be between 5-25 characters long.";
+		send(newSock,response,strlen(response),0);
+		return;
+	}
+	else
+	{
+		box* new = (box*)malloc(sizeof(box));
+		new->name=name;
+		new->next=NULL;
+		add(new);
+	}
+}
+
+void open(char* name)
+{
+	char* response;
+	send(newSock,response,strlen(response),0);
+		
+	if(name==NULL)
+	{
+		response="You need a name, dude.";
+		send(newSock,response,strlen(response),0);
+		return;
+	}
+	if(!isalpha(name[0])
+	{
+		response="You need to start your name with an alphabetical character.";
+		send(newSock,response,strlen(response),0);
+		return;
+	}
+	if(strlen(name)<5||strlen(name)>25)
+	{
+		response="Your name must be between 5-25 characters long.";
+		send(newSock,response,strlen(response),0);
+		return;
+	}
+	else
+	{
+		box* curr = getBox(name);
+		if(curr==NULL)
+		{
+			response = "That name doesn't exist";
+			send(newSock,response,strlen(response),0);
+			return;
+	}
+}
+
+int main(int argc, char const *argv[])
 { 
 	char command[1024];
-	int serverFD,newSock,rval,opt,addrLen; 
+	int serverFD,newSock,rval,opt,addrLen,status; 
 	opt=1;
         struct sockaddr_in addr; 
 	addrLen=sizeof(addr);
@@ -41,11 +149,66 @@ int main(int argc, char const *argv[])
     	{ 
         	perror("accept"); 
         	exit(EXIT_FAILURE); 
-    	} 
-    	rval=read(newSock,command,1024); 
-    	printf("%s\n",command);
-    	char*hello="HELLO FROM SERVER";
-    	send(newSock,hello,strlen(hello),0); 
-    	printf("Hello message sent\n"); 
+    	}     	
+	/*
+			Commands:
+			HELLO_
+			GDBYE_
+			CREAT_
+			OPNBX_
+			NXTMG_
+			PUTMG_
+			DELBX_
+			CLSBX_
+	*/
+	rval=read(newSock,command,1024);	
+	char cmd[6];
+	char content[1018];
+	char* response;
+	printf("%s\n",command);
+	if(!strcmp(command,"HELLO"))
+	{
+		response="HELLO DUMBv0 ready!";
+		send(newSock,response,strlen(response),0);
+		while(1)
+		{	
+			rval=recv(newSock,command,1024,0);
+			strncpy(cmd,command,6);
+			strncpy(content,command+6,1024);
+			printf("%s\n",cmd);
+			if(!strcmp(command,"GDBYE"))
+			break;
+			else if(!strcmp(cmd,"CREAT "))
+			{
+				
+			}
+			else if(!strcmp(cmd,"OPNBX "))
+			{
+				
+			}
+			else if(!strcmp(cmd,"NXTMG "))
+			{
+				
+			}
+			else if(!strcmp(cmd,"PUTMG "))
+			{
+				
+			}
+			else if(!strcmp(cmd,"DELBX "))
+			{
+				
+			}
+			else if(!strcmp(cmd,"CLSBX "))
+			{
+				
+			}
+
+		}
+	}
+	else
+	{
+		response="Error: Incorrect Greetings";
+		send(newSock,response,strlen(response),0);
+	}
     	return 0; 
 } 
